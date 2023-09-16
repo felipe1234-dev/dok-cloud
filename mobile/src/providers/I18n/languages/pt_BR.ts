0b45f0b9d5c1a3e843359826f6dc0a2b419fa441
Felipe Alves <felipe.alves@taugor.com.br>
2023-09-16 12:32:39 -0300

    add(providers/i18n): add translations

--- mobile/src/providers/I18n/languages/pt_BR.ts
+++ mobile/src/providers/I18n/languages/pt_BR.ts
@@ -8,7 +8,10 @@ const pt_BR: TranslationTable = {
     "Manage your files in a simple way":
         "Gerencie seus arquivos em uma maneira simples",
     "Storage and secure your files in the cloud infinitely with password and fingerprint":
-        "Armazene e proteja seus arquivos na nuvem com espaço infinito com senhas e digital"
+        "Armazene e proteja seus arquivos na nuvem com espaço infinito com senhas e digital",
+    Login: "Entrar",
+    "example@example.com": "exemplo@exemplo.com",
+    Password: "Senha",
 };
 
 export { pt_BR };
