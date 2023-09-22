606ac4940f525b55d128f751ef3d9b458df11a8b
Felipe Alves <felipe.alves@taugor.com.br>
2023-09-21 23:46:34 -0300

    refactor(services/LocalStorage): implement a class that unifies AsyncLocalStorage (mobile) and localStorage (web) for testing purposes

--- mobile/src/services/LocalStorage.ts
+++ mobile/src/services/LocalStorage.ts
@@ -1,1 +1,28 @@
-export { default as LocalStorage } from "@react-native-async-storage/async-storage";
+import AsyncLocalStorage from "@react-native-async-storage/async-storage";
+
+class LocalStorage {
+    static async getItem(key: string) {
+        const value =
+            (await AsyncLocalStorage.getItem(key)) || !!localStorage
+                ? localStorage.getItem(key)
+                : null;
+        return value;
+    }
+
+    static async setItem(key: string, value: string) {
+        await AsyncLocalStorage.setItem(key, value);
+        localStorage.setItem(key, value);
+    }
+
+    static async removeItem(key: string) {
+        await AsyncLocalStorage.removeItem(key);
+        localStorage.removeItem(key);
+    }
+
+    static async clear() {
+        await AsyncLocalStorage.clear();
+        localStorage.clear();
+    }
+}
+
+export { LocalStorage };
