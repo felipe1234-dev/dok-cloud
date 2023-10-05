a762292393589ee116df3978d7572f28f3fe0a84
Felipe Alves <felipe.alves@taugor.com.br>
2023-10-05 00:03:32 -0300

    fix(services/LocalStorage): check if platform is mobile before using localStorage global variable

--- mobile/src/services/LocalStorage.ts
+++ mobile/src/services/LocalStorage.ts
@@ -1,26 +1,28 @@
 import AsyncLocalStorage from "@react-native-async-storage/async-storage";
+import { Platform } from "react-native";
 
 class LocalStorage {
     static async getItem(key: string) {
-        const value = localStorage
-            ? localStorage.getItem(key)
-            : await AsyncLocalStorage.getItem(key);
+        const value =
+            Platform.OS === "web"
+                ? localStorage.getItem(key)
+                : await AsyncLocalStorage.getItem(key);
         return value;
     }
 
     static async setItem(key: string, value: string) {
         await AsyncLocalStorage.setItem(key, value);
-        if (localStorage) localStorage.setItem(key, value);
+        if (Platform.OS === "web") localStorage.setItem(key, value);
     }
 
     static async removeItem(key: string) {
         await AsyncLocalStorage.removeItem(key);
-        if (localStorage) localStorage.removeItem(key);
+        if (Platform.OS === "web") localStorage.removeItem(key);
     }
 
     static async clear() {
         await AsyncLocalStorage.clear();
-        if (localStorage) localStorage.clear();
+        if (Platform.OS === "web") localStorage.clear();
     }
 }
 
