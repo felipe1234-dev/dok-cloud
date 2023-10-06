1c3577345b3c7a0d7df65e13fe558d8a8b904d31
Felipe Alves <felipe.alves@taugor.com.br>
2023-10-06 20:58:26 -0300

    add(assets/images): replace empty-box image.png and remove empty-icon.png

--- mobile/src/assets/images/index.ts
+++ mobile/src/assets/images/index.ts
@@ -2,5 +2,4 @@ export const WELCOME_SCREEN = require("./welcome-screen.png");
 export const LOGIN_SCREEN = require("./login-screen.png");
 export const REGISTER_SCREEN = require("./register-screen.png");
 export const FOLDER_ICON = require("./folder-icon.png");
-export const EMPTY_ICON = require("./empty-icon.png");
 export const EMPTY_BOX = require("./empty-box.png");
