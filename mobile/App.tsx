8eff621c06c5421fbc3774e51ce9ec740fd51f93
Felipe Alves <felipe.alves@taugor.com.br>
2023-10-07 20:30:04 -0300

    fix(App): reorder providers

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { Composer } from "./src/components";
 import {
-    I18nProvider,
-    ThemeProvider,
     LoaderProvider,
-    ToastProvider,
-    NavigatorProvider,
     AuthProvider,
+    I18nProvider,
+    ToastProvider,
+    ThemeProvider,
+    NavigatorProvider,
     CloudProvider,
 } from "./src/providers";
 import Root from "./src/Root";
 
 function App() {
     const providers = [
-        I18nProvider,
-        ThemeProvider,
         LoaderProvider,
-        ToastProvider,
-        NavigatorProvider,
         AuthProvider,
+        I18nProvider,
+        ToastProvider,
+        ThemeProvider,
+        NavigatorProvider,
         CloudProvider,
     ];
 
