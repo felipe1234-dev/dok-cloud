70fc11ee16a1f49683ac52817a4780f1a11ab4ca
Felipe Alves <felipe.alves@taugor.com.br>
2023-10-08 00:20:58 -0300

    doc: add comments

--- mobile/src/types/ScreenConfig.ts
+++ mobile/src/types/ScreenConfig.ts
@@ -4,10 +4,14 @@ import { IconProps } from "@components";
 type ScreenName = "Welcome" | "Login" | "Register" | "Home" | "Cloud";
 
 interface ScreenConfig {
+    /** Whether this screen will appear first */
     index?: boolean;
+    /** Unique identifier */
     name: ScreenName;
     component: (props: ScreenProps) => JSX.Element;
+    /** Icon props of the icon that will appear in the BottomNav component */
     icon?: IconProps;
+    /** Whether this screen requires user authentication to be viewed */
     protected: boolean;
 }
 
