a22e31f8999cc787ea64b50898097d9093914691
Felipe Alves <felipe.alves@taugor.com.br>
2023-10-08 00:45:16 -0300

    add(constants): add constant for the max allowed chunk size (= to Firestore document max size)

--- globals/src/constants/index.ts
+++ globals/src/constants/index.ts
@@ -16,3 +16,6 @@ export const KB = 1000;
 export const MB = 1000 * KB;
 export const GB = 1000 * MB;
 export const TB = 1000 * GB;
+export const KiB = 1024;
+export const MiB = 1024 * KiB;
+export const MAX_CHUNK_SIZE = 1 * MiB - 89;
